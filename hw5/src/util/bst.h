03cc8978c59938b7487b90ea21dbdcdbb1bc991d
freedom0116 <b04505016@ntu.edu.tw>
2019-11-19 00:44:12 +0800

    fix erase

--- hw5/src/util/bst.h
+++ hw5/src/util/bst.h
@@ -53,6 +53,7 @@ public:
    }
 
    int size(){ return _nodelist.size(); }
+
    bool empty(){
       if(this->size() == 0) return true;
       else return false;
@@ -71,8 +72,10 @@ class BSTree
 {
    // TODO: design your own class!!
 public:
-   BSTree(): _root(0), _size(0) {}
-   ~BSTree() {clear(); delete _root; }
+   BSTree(): _root(0), _size(0) {
+      _BSend = new BSTreeNode<T>(T());
+   }
+   ~BSTree() {clear(); delete _BSend; }
 
    class iterator
    { 
@@ -86,44 +89,32 @@ public:
       const T& operator * () const { return _node->_data; }
       T& operator * () { return _node->_data; }
       iterator& operator ++ () {
-         BSTreeNode<T>* n;
-         string state;
          if(list.empty()){
-            _node = _node->_top;
-            n = _node;
-            state = "left";
+            if(_node->_top == _node){
+               _node = _node->_right;
+            } // _root
+            else{
+               if(_node->_right == 0){
+                  _node = _node->_top;
+               }else{
+                  list.push_back(_node->_top);
+                  _node = localmin(_node->_right);
+               }
+            }
          }else{
-            _node = list.get_node(list.size() - 1);
-            state = list.get_state(list.size() - 1);
-            n = _node;     // return this one
+            _node = list[list.size() - 1]; // return this one
             list.pop_back();
          }
          
-         if(state == "left"){
-            if(n->_top == n){
-               list.push_back(n->_right, "top");
-            } // _root
-            else{
-               list.push_back(n->_top, "left");
-               if(n->_right != 0){
-                  list.push_back(n->_right, "top");
-               }
-            }
-         }else if(state == "top"){
-            int children = 0;
-            if(n->_left != 0) children += 1;
-            else if(n->_right != 0) children += 2;
-
-            if(children == 1){
-               list.push_back(n->_left, "top");
-            }else if(children == 2){
-               list.push_back(n->_right, "top");
-            }else if(children == 3){
-               list.push_back(n->_right, "top");
-               list.push_back(n->_left, "top");
-            }
+         if(_node->_top != _node){
+            BSTreeNode<T>* t = _node->_top;
+            if(t->_right != _node) list.push_back(_node->_top);
          }
-
+         if(_node->_right != 0){
+            BSTreeNode<T>* i;
+            i = localmin(_node->_right);
+            list.push_back(i);
+         }
          return *this;
       } // move to next
       iterator operator ++ (int) {
@@ -139,6 +130,7 @@ public:
          while(_node->_right != 0){
             _node = _node->_right;
          }
+         _node = _node->_top;
          return *this;
       } // move to end (rightmost)
       iterator operator -- (int) {
@@ -164,7 +156,13 @@ public:
 
    private:
       BSTreeNode<T>* _node; // store init node
-      List<BSTreeNode<T>*, string> list; // only 2 state: left, top
+      vector<BSTreeNode<T>*> list; // state: node come from      
+
+      BSTreeNode<T>* localmin(BSTreeNode<T>* n){
+         while(n->_left != 0)
+            n = n->_left;
+         return n;
+      }
    };
 
    iterator begin() const {
@@ -174,17 +172,14 @@ public:
       return n;
    }
    iterator end() const {
-      iterator n;
-      --n;
-      return n;
+      iterator e(_BSend);
+      return e;
    }
    bool empty() const {
       if(_root == 0) return true;
       return false;
    }
-   size_t size() const { 
-      return _size;
-   }
+   size_t size() const { return _size; }
 
    void insert(const T& x) {
       BSTreeNode<T>* newnode = new BSTreeNode<T>(x);
@@ -195,23 +190,24 @@ public:
          int direction;
          BSTreeNode<T>* i = _root;
          while(true){
-            direction = compare(i->_data, newnode->_data);
+            direction = compare(i->_data, x);
             if(direction == -1){
                if(i->_left == 0) {
                   i->_left = newnode;
                   newnode->_top = i;
-               }else 
-                  i = i->_left;
+                  break;
+               }else i = i->_left;
             }
             else{
-               if(i->_right == 0){
+               if(i->_right == 0 || i->_right == _BSend){
                   i->_right = newnode;
                   newnode->_top = i;
-               }else 
-                  i = i->_right;
+                  break;
+               }else i = i->_right;
             }
          }
       }
+      setend();
       _size += 1;
    }
    void pop_front() {
@@ -226,11 +222,18 @@ public:
             _root = _root->_right;
             _root->_top = _root;
          }else{
-            head = head->_top;
-            head->_left = 0;
+            if(head->_right == 0){
+               head = head->_top;
+               head->_left = 0;
+            }else{
+               head = head->_right;
+               d->_top->_left = head;
+               head->_top = d->_top;
+            }
          }
          delete d;
       }
+      setend();
       _size -= 1;
    }
    void pop_back() {
@@ -239,78 +242,111 @@ public:
          delete _root;
          _root = NULL;         
       }else{
-         iterator i = end();
-         if(i._node == _root) {
+         BSTreeNode<T>* d = _BSend->_top;
+         BSTreeNode<T>* tail = d;
+         if(tail == _root) {
             _root = _root->_left;
             _root->_top = _root;
          }else{
-            BSTreeNode<T>* tail = i._node;
-            tail = tail->_top;
-            tail->_right = 0;
+            if(tail->_left == 0){
+               tail = tail->_top;
+               tail->_right = 0;
+            }else{
+               tail = tail->_left;
+               d->_top->_right = tail;
+               tail->_top = d->_top;
+            }
          }
-         delete i._node;  
+         delete d;  
       }
+      setend();
       _size -= 1;
    }
 
    // return false if nothing to erase
    bool erase(iterator pos) {
       if(pos._node == 0) return false;
+      _size -= 1;
       if(size() == 1){
          delete _root;
          _root = NULL;
       }else
          repair(pos._node);
+      setend();
       return true;
    }
    bool erase(const T& x) {
       iterator n;
       n = find(x);
       if(n._node == 0) return false;
+      _size -= 1;
       if(size() == 1){
          delete _root;
          _root = NULL;
       }else
          repair(n._node);
+      setend();
       return true;
    }
 
    iterator find(const T& x) {
-      int direction;
-      BSTreeNode<T>* i = _root;
       if(empty()) return iterator();
-      while(true){
-         direction = compare(i->_data, x);
-         if(direction == -1){
-            if(i->_left != 0) i = i->_left;
-            else return iterator(); 
-         }
-         else if(direction == 0){
-            iterator n(i);
-            i = NULL;
-            return n;
-         } // find
-         else if(direction == 1){
-            if(i->_right != 0) i = i->_right;
-            else return iterator();
-         }
-         else{
-            cerr << "Error: function compare() isn't correct" << endl;
-            throw exception();
-         }
+      BSTree<T>::iterator i = begin();
+      for(; i != end(); ++i){
+         if(*i == x) return i;
       }
+      return iterator();
+      // int direction;
+      // BSTreeNode<T>* i = _root;
+      // while(true){
+      //    direction = compare(i->_data, x);
+      //    if(direction == -1){
+      //       if(i->_left != 0) i = i->_left;
+      //       else return iterator(); 
+      //    }
+      //    else if(direction == 0){
+      //       iterator n(i);
+      //       i = NULL;
+      //       return n;
+      //    } // find
+      //    else if(direction == 1){
+      //       if(i->_right != 0) i = i->_right;
+      //       else return iterator();
+      //    }
+      //    else{
+      //       cerr << "Error: function compare() isn't correct" << endl;
+      //       throw exception();
+      //    }
+      // }
    }
 
    void clear() {
-
+      if(!empty()){
+         BSTree<T>::iterator i = begin();
+         for(; i != end(); i++){
+            delete i._node;
+         }
+      }
    } // remember to do _head = NULL
 
-   void print() const {}
+   void print() const {
+      // BSTree<T>::iterator i = begin();
+      // int idx = 0;
+      // if(!empty()){
+      //    for(; i != end(); i++){
+      //       cout << "[" << setw(3) << right << idx << "] = "
+      //       << setw(3) << right << *i << "   ";
+      //       if(idx % 4 == 3) cout << endl;
+      //    }
+      //    if(idx % 4 == 3) cout << endl;
+      // }
+   }
 
    void sort() const {} // useless
 
 private:
    BSTreeNode<T>* _root;
+   BSTreeNode<T>* _BSend;
    size_t _size;
 
    BSTreeNode<T>* head() const {
@@ -351,6 +387,20 @@ private:
       alter->_left = n->_left;
       alter->_right = n->_right;
       delete n;
+   }
+
+   void setend(){
+      if(empty()){
+         _BSend->_top = NULL;
+      }
+      BSTreeNode<T>* i = _root;
+      while(i->_right != 0){
+         i = i->_right;
+      }
+      if(i != _BSend){
+         _BSend->_top = i;
+         i->_right = _BSend;
+      }
    }
 };
 
